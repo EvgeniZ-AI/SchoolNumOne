089d281fc243ce6bbb5500801a54d2bbe7f34ded
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-04 02:38:46 +1000

    Добавьте файлы проекта.

---
+++ SchoolNumOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolNumOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сделать подключение к бд через entity
+            //Обозначить все классы в школе
+            //добавить в каждый класс учеников 
+            //добавить каждому ученику оценки
+            //вызывать запросы при помощи Linq
+        }
+    }
+}
