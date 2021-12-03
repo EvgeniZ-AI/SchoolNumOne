da1dc7ea1c91b2f69233abd52a4781efd6d3555b
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-04 02:57:28 +1000

    добавил класс учеников

---
+++ SchoolNumOne/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolNumOne
+{
+    class Student
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int classId { get; set; }
+    }
+}
