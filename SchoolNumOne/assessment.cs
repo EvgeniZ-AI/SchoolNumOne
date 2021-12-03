8995640b317a3d387ba6a4408e7af79ad6d7a452
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-04 03:03:54 +1000

    добавил класс с оценками

---
+++ SchoolNumOne/assessment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolNumOne
+{
+    class assessment
+    {
+        public int id { get; set; }
+        public int[] assessments { get; set; }
+    }
+}
